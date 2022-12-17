3f1cce83b02cb62813c0a9f9289427bd67e9e1a5
sandy yadav <velpulasandeep321@gmail.com>
2022-12-17 04:01:46 +0000

    hi

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+  //  Write your code here.
+  <div className="main-div">
+    <Button>Like</Button>
+    <Button>Comment</Button>
+    <Button>Share</Button>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
